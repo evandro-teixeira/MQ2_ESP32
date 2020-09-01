da7e1a7c17c5e6d3864a66b5a5742caa442821dd
evandro-teixeira <evandrolst@live.com>
2020-09-01 01:25:38 -0300

    bug fix

--- mq2.cpp
+++ mq2.cpp
@@ -110,7 +110,7 @@ void mq2::begin(void)
     {
         case Channel_ADC1:
             adc1_config_width(ADC_WIDTH_BIT_12);
-            adc1_config_channel_atten((adc1_channel_t)ADC1_CHANNEL_0,ADC_ATTEN_DB_11);
+            adc1_config_channel_atten((adc1_channel_t)ADC_PIN_MAP[index].pin_adc,ADC_ATTEN_DB_11);
         break;
         case Channel_ADC2:
             adc2_config_channel_atten((adc2_channel_t)ADC_PIN_MAP[index].pin_adc, ADC_ATTEN_DB_11 );
